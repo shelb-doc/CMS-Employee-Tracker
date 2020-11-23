8763aab9ff132d41e2c7bbc496fc952aa4fb9ce7
Shelby De Oliveira Cignetti <shelbycignetti@gmail.com>
2020-11-23 18:21:57 -0500

    added ASCII title

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ const inquirer = require('inquirer');
 
 // Creates ASCII Art from text
 const figlet = require('figlet');
+
+// Title Art
+figlet('CMS Employee Tracker', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
