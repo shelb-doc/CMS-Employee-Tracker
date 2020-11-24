9c218ba6d226afc66d82a513f66f81e09b72b7c4
Shelby De Oliveira Cignetti <shelbycignetti@gmail.com>
2020-11-24 17:58:42 -0500

    created and tested the table for Role

--- db/schema.sql
+++ db/schema.sql
@@ -8,3 +8,12 @@ CREATE TABLE department (
   name VARCHAR(50) NOT NULL,
   PRIMARY KEY (id)
 );
+
+CREATE TABLE role (
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  title VARCHAR(50) NOT NULL,
+  salary DECIMAL(15,2) NOT NULL,
+  department_id INTEGER,
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+);
