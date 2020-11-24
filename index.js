1621a303e8ab0b1022e43f2ef6621756b2445d83
Shelby De Oliveira Cignetti <shelbycignetti@gmail.com>
2020-11-23 19:19:43 -0500

    made mysql connection

--- index.js
+++ index.js
@@ -5,6 +5,23 @@ const inquirer = require('inquirer');
 // Creates ASCII Art from text
 const figlet = require('figlet');
 
+// Internal Modules
+
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "CMS-Tracker-App",
+    password: "password",
+    database: "cms_employee_tracker"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    start();
+});
+
+
 // Title Art
 figlet('CMS Employee Tracker', function(err, data) {
     if (err) {
