ea37571ab44271c0cb27665d898d722386d1d8a5
Shelby De Oliveira Cignetti <shelbycignetti@gmail.com>
2020-11-23 21:02:43 -0500

    Commeted out title art due to error added inquirer prompts for ADD and QUIT

--- index.js
+++ index.js
@@ -22,12 +22,50 @@ connection.connect(function (err) {
 });
 
 
+
+
+// Inquirer Prompt
+
+function start() {
+    inquirer.prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            "Add Employee",
+            "Add Role",
+            "Add Department",
+            "QUIT"
+        ]
+    })
+    .then(function (answer) {
+        switch (answer.action){
+
+            case "Add Employee":
+                add.addEmployee(connection, start);
+                break;
+            
+            case "Add Employee":
+                add.addRole(connection, start);
+                break;
+            
+            case "Add Employee":
+                add.addRole(connection, start);
+                break;
+
+            case "QUIT":
+                connection.end();
+                break;
+        }
+    })
+}
+
 // Title Art
-figlet('CMS Employee Tracker', function(err, data) {
-    if (err) {
-        console.log('Something went wrong...');
-        console.dir(err);
-        return;
-    }
-    console.log(data)
-});
+// figlet('CMS Employee Tracker', function(err, data) {
+//     if (err) {
+//         console.log('Something went wrong...');
+//         console.dir(err);
+//         return;
+//     }
+//     console.log(data)
+// });
