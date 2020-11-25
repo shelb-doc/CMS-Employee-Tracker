0443ab1171c8701d39a4930b496cb52436aa924f
Shelby De Oliveira Cignetti <shelbycignetti@gmail.com>
2020-11-24 20:12:57 -0500

    added Role Seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -10,3 +10,19 @@ VALUES
 ("Sales");
 
 SELECT * FROM department;
+
+-- Role Seed's
+
+INSERT INTO role (title, salary, department_id)
+VALUES 
+("Regional Manager", 180000.00, 1),
+("Assistant to the Regional Manager", 90000.00, 1),
+("Accountant", 70000.00, 2),
+("HR Associate", 90000.00, 3),
+("Software Engineer", 175000.00, 4),
+("Administrative Professional", 40000.00, 5),
+("Manager", 80000.00, 6),
+("Quality Control Associate", 45000.00, 6),
+("Sales Representative", 60000.00, 6);
+
+SELECT * FROM role;
