3b7b9bd4db9a88c7430f7c0b77bbeb54f2905b54
Shelby De Oliveira Cignetti <shelbycignetti@gmail.com>
2020-11-24 19:33:49 -0500

    added seeds file and inserterd  departments to the table

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+-- Department Seed's
+INSERT INTO department (name)
+VALUES
+
+("Executive"), 
+("Finance"), 
+("Human Resources"), 
+("Information Technology"), 
+("Operations"), 
+("Sales");
+
+SELECT * FROM department;
